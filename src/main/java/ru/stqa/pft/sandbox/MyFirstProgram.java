601053df463637625c75598105ef11c70b3fa278
AnastasiaDorogva <drumnnastya@gmail.com>
2021-07-11 20:28:44 +0300

    отступы

--- src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,1 @@
-package ru.stqa.pft.sandbox;
-
-public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
+package ru.stqa.pft.sandbox;public class MyFirstProgram {    public static void main(String[] args) {        System.out.println("Hello world");    }}
