c3c5dcc3861cb8013d0058544dd8e1a63b92c592
AnastasiaDorogva <drumnnastya@gmail.com>
2021-08-03 22:21:54 +0300

    Лекция 4.8. Поиск максимального элемента в коллекции

--- addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,18 +4,19 @@ import java.util.Objects;
 
 public class GroupData {
   private final String name;
-  private final String id;
+
+  private int id;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
@@ -30,22 +31,12 @@ public class GroupData {
     return footer;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData data = (GroupData) o;
-    return Objects.equals(name, data.name) &&
-            Objects.equals(id, data.id);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name, id);
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getName() {
@@ -58,5 +49,18 @@ public class GroupData {
             "name='" + name + '\'' +
             ", id='" + id + '\'' +
             '}';
+  }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData data = (GroupData) o;
+    return id == data.id &&
+            Objects.equals(name, data.name);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, id);
   }
 }
