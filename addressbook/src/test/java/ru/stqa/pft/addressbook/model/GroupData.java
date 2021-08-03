5c967556538c778a64e698d9790e31135c69ef28
AnastasiaDorogva <drumnnastya@gmail.com>
2021-08-03 22:06:04 +0300

    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -4,37 +4,22 @@ import java.util.Objects;
 
 public class GroupData {
   private final String name;
+  private final String id;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getName() {
-    return name;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    GroupData data = (GroupData) o;
-    return Objects.equals(name, data.name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(name);
-  }
-
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "name='" + name + '\'' +
-            '}';
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
   }
 
   public String getHeader() {
@@ -43,5 +28,35 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData data = (GroupData) o;
+    return Objects.equals(name, data.name) &&
+            Objects.equals(id, data.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, id);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            ", id='" + id + '\'' +
+            '}';
   }
 }
