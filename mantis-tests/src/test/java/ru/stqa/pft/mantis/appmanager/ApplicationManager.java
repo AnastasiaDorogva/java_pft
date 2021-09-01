520ea879ab7dfc5f70557875d0ca8706d1483962
AnastasiaDorogva <drumnnastya@gmail.com>
2021-09-01 23:46:29 +0300

    лекция 8.3 Добавление помощника Добавление апи теста на авторизацию закометила инциализацию браузера добавлены зависимоти для httpClient

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -9,35 +9,45 @@ import org.openqa.selenium.remote.BrowserType;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  public WebDriver wd;
+//  public WebDriver wd;
 
-  private final String browser;
+//  private final String browser;
 
   public ApplicationManager(String browser) {
-    this.browser = browser;
+//    this.browser = browser;
     properties = new Properties();
   }
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-    switch (browser) {
-      case BrowserType.CHROME -> wd = new ChromeDriver();
-      case BrowserType.FIREFOX -> wd = new FirefoxDriver();
-      case BrowserType.IE -> wd = new InternetExplorerDriver();
-    }
-    wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+//    switch (browser) {
+//      case BrowserType.CHROME -> wd = new ChromeDriver();
+//      case BrowserType.FIREFOX -> wd = new FirefoxDriver();
+//      case BrowserType.IE -> wd = new InternetExplorerDriver();
+//    }
+//    wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+//    wd.get(properties.getProperty("web.baseUrl"));
+//
+//  }
+//
+//  public void stop() {
+//    wd.quit();
   }
 
-  public void stop() {
-    wd.quit();
+  public HttpSession newSession()  {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+   return properties.getProperty(key);
   }
 }
