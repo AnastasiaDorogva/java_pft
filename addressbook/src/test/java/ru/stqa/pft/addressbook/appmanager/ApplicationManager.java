3895352184a1f6f9141b0b31436b3f64a7364cd2
AnastasiaDorogva <drumnnastya@gmail.com>
2021-09-17 18:34:02 +0300

    .

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,10 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -22,53 +25,57 @@ public class ApplicationManager {
   private final String browser;
   private DBHelper dbHelper;
 
-  public ApplicationManager(String browser)  {
+  public ApplicationManager(String browser) {
     this.browser = browser;
     properties = new Properties();
   }
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
     dbHelper = new DBHelper();
-
-    if (browser.equals(BrowserType.CHROME)) {
-      System.setProperty("webdriver.chrome.driver", properties.getProperty("webdriver.chrome"));
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)) {
-      System.setProperty("webdriver.ie.driver", properties.getProperty("webdriver.ie"));
-    } else if (browser.equals(BrowserType.FIREFOX)) {
-      System.setProperty("webdriver.gecko.driver", properties.getProperty("webdriver.firefox"));
-      wd = new FirefoxDriver();
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
-    wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    contactHelper = new ContactHelper(wd);
-    SessionHelper sessionHelper = new SessionHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
+      wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+      groupHelper = new GroupHelper(wd);
+      navigationHelper = new NavigationHelper(wd);
+      contactHelper = new ContactHelper(wd);
+      SessionHelper sessionHelper = new SessionHelper(wd);
+      sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+    }
   }
 
-  public void stop() {
-    wd.quit();
-  }
+    public void stop () {
+      wd.quit();
+    }
 
-  public GroupHelper group() {
-    return groupHelper;
-  }
+    public GroupHelper group () {
+      return groupHelper;
+    }
 
-  public NavigationHelper goTo() {
-    return navigationHelper;
-  }
+    public NavigationHelper goTo () {
+      return navigationHelper;
+    }
 
-  public ContactHelper contact() {
-    return contactHelper;
-  }
+    public ContactHelper contact () {
+      return contactHelper;
+    }
 
-  public DBHelper db() {
-    return dbHelper;
-  }
+    public DBHelper db () {
+      return dbHelper;
+    }
 
-}
+  }
